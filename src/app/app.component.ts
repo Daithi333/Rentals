6fc6f6455b40490af33762746787e48cd1664afa
David McElhill <dmcelhill01@qub.ac.uk>
2019-06-18 10:23:37 +0100

    check auth state on app resume added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Platform } from '@ionic/angular';
-import { Plugins, Capacitor } from '@capacitor/core';
+import { Plugins, Capacitor, AppState } from '@capacitor/core';
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 
 import { AuthService } from './auth/auth.service';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -37,6 +38,7 @@ export class AppComponent implements OnInit, OnDestroy {
       }
       this.previousAuthState = isAuth;
     });
+    Plugins.App.addListener('appStateChange', this.checkAuthOnResume.bind(this));
   }
 
   onLogout() {
@@ -46,6 +48,19 @@ export class AppComponent implements OnInit, OnDestroy {
   ngOnDestroy() {
     if (this.authSub) {
       this.authSub.unsubscribe();
+    }
+  }
+
+  private checkAuthOnResume(state: AppState) {
+    if (state.isActive) {
+      this.authService
+        .autoLogin()
+        .pipe(take(1))
+        .subscribe(success => {
+          if (!success) {
+            this.onLogout();
+          }
+        });
     }
   }
 
